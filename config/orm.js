ab35c579cd750918a2539f6b7e22772766018ae8
angelpsch <angelpsch@users.noreply.github.com>
2020-08-06 08:21:12 -0500

    orm

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+const connection = require(`./connection`);
+
+const orm = {
+    selectAll: function (input, cb) {
+        const qString = `SELECT * FROM ${input};`;
+        connection.query(qString, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        const qString = `UPDATE ${table} SET devoured = ${objColVals} WHERE ${condition}`;
+
+        connection.query(qString, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        })
+    },
+    insertOne: function (table, cols, vals, cb) {
+        const qString = `INSERT INTO ${table} (${cols.toString()}) VALUES (?)`;
+
+        connection.query(qString, vals, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    }
+
+
+}
+
+module.exports = orm;
