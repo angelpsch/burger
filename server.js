7a72896e23dcdedfb5d41a0506e5c2b26a5bff3b
angelpsch <angelpsch@users.noreply.github.com>
2020-08-06 10:34:02 -0500

    forgot to add this back

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller");
 
 var PORT = process.env.PORT || 8080;
 
@@ -10,12 +12,12 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller");
+
 
 app.use(routes);
 
